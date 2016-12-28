d602a2a7fcc5f9dbb1f3df00eb3beed6c713dbd4
Mike Foley <mfoley@manh.com>
2016-12-28 17:17:05 -0500

    initial checkin with searchguard test

--- kibana/entrypoint.sh
+++ kibana/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Change kibana pink color to white
+echo "Change color..."
+sed -i 's/\@kibanaPink1:                           #E8488B;/\@kibanaPink1:                           \@white;/' /usr/share/kibana/src/ui/public/styles/variables/colors.less
+
 # Wait for the Elasticsearch container to be ready before starting Kibana.
 echo "Stalling for Elasticsearch"
 while true; do
